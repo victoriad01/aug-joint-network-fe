dfe5290975f86f6d040f85cd80e2e402d8897093
victoriad01 <94958284+victoriad01@users.noreply.github.com>
2023-05-31 17:59:40 +0100

    added onchange on select prayer request type

---
+++ src/pages/RequestPrayer/Popuprequest/Popuprequest.tsx
@@ -0,0 +1,44 @@
+import { useNavigate } from 'react-router-dom'
+
+interface PopupProps {
+  setSubmit: (value: boolean) => void
+  setSubmitted: (value: boolean) => void
+}
+
+export const Popuprequest = ({ setSubmit, setSubmitted }: PopupProps) => {
+  const navigate = useNavigate()
+
+  return (
+    <div className='mx-[24px] md:mx-[160px]  lg:mx-[360px] mt-[115px] shadow-lg p-8'>
+      <div>
+        <p className='text-[18px] md:text-[46px] text-center font-EuclidMedium mb-2 md:mb-[24px] leading-11'>
+          Prayer Submitted Successfully
+        </p>
+        <p className='text-[10px] md:text-[18px] text-center font-Euclidlight'>
+          Your prayer request has been directed to the appropriate channel.
+        </p>
+        <div
+          className=' flex justify-center items-center'
+          onClick={() => {
+            setSubmit(true)
+            setSubmitted(false)
+          }}
+        >
+          <button className='my-[16px] md:my-[30px] w-3/4 cursor-pointer px-[24px] py-[12px] bg-[#9A4797] text-[white] rounded-md mb-[2px] font-EuclidMedium shadow-md'>
+            Submit New Prayer
+          </button>
+        </div>
+        <div
+          className=' flex justify-center items-center'
+          onClick={() => {
+            navigate('/')
+          }}
+        >
+          <button className='my-[8px] md:my-[4px] w-3/4 cursor-pointer px-[24px] py-[12px] border-[1px] border-[#9A4797] text-[#9A4797] bg-[white] rounded-md mb-[34px] font-EuclidMedium shadow-md'>
+            Return Home
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/RequestPrayer/Requestprayer.tsx
+++ src/pages/RequestPrayer/Requestprayer.tsx
@@ -1,4 +1,5 @@
 import { ChangeEvent, useState } from 'react'
+import { Popuprequest } from './Popuprequest/Popuprequest'
 
 const optionData = [
   { id: 1, title: 'Soul Winning (Evangelism)' },
@@ -18,44 +19,72 @@ export const Requestprayer = () => {
   const options = optionData.map((p, i) => p.title)
 
   const [text, setText] = useState('')
-  console.log(text)
+  const [submit, setSubmit] = useState(true)
+  const [submitted, setSubmitted] = useState(false)
+
   const handleTextChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     setText(event.target.value)
   }
+
+  const [selectedValue, setSelectedValue] = useState('')
+
+  const handleSelectChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setSelectedValue(event.target.value)
+    console.log(selectedValue)
+  }
+
   return (
-    <div className='mx-[24px] md:mx-[160px]  lg:mx-[360px] my-[115px]'>
-      <p className='text-center text-[28px] md:text-[36px] lg:text-[56px] font-EuclidMedium lg:leading-[67.2px]'>
-        Submit Prayer Request
-      </p>
-      <input
-        type='text'
-        placeholder='Name (optional)'
-        className='w-full px-[24px] py-[12px] border-[1px] border-[#9A4797] text-[#9A4797] rounded-md my-[42px] font-EuclidMedium '
-      />
+    <div>
+      {submit ? (
+        <div className='mx-[24px] md:mx-[160px]  lg:mx-[360px] my-[115px]'>
+          <p className='text-center text-[28px] md:text-[36px] lg:text-[56px] font-EuclidMedium lg:leading-[67.2px]'>
+            Submit Prayer Request
+          </p>
+          <input
+            type='text'
+            placeholder='Name (optional)'
+            className='w-full px-[24px] py-[12px] border-[1px] border-[#9A4797] text-[#9A4797] rounded-md my-[42px] font-EuclidMedium '
+          />
 
-      <div className='w-full px-[24px] py-[12px] border-[1px] border-[#9A4797] text-[#9A4797] rounded-md my-[2px] font-EuclidMedium '>
-        <select className='w-full outline-none bg-white'>
-          {options.map((n, i) => (
-            <option key={n} className=' bg-white'>
-              {n}
-            </option>
-          ))}
-        </select>
-      </div>
+          <div className='w-full px-[24px] py-[12px] border-[1px] border-[#9A4797] text-[#9A4797] rounded-md my-[2px] font-EuclidMedium '>
+            <select
+              className='w-full outline-none bg-white'
+              onChange={handleSelectChange}
+              value={selectedValue}
+            >
+              {options.map((n, i) => (
+                <option key={n} value={n} className=' bg-white'>
+                  {n}
+                </option>
+              ))}
+            </select>
+          </div>
 
-      <textarea
-        value={text}
-        onChange={handleTextChange}
-        placeholder='Enter your prayer request here'
-        className='h-[150px] w-full px-[24px] py-[12px] border-[1px] border-[#9A4797] text-[#9A4797] rounded-md my-[42px] font-EuclidMedium outline-none text-justify'
-      />
-      <button
-        onClick={() => {}}
-        className='w-full cursor-pointer px-[24px] py-[12px] bg-[#9A4797] text-[white] rounded-md my-[12px] font-EuclidMedium shadow-md'
-        disabled={text.length < 0}
-      >
-        Submit
-      </button>
+          <textarea
+            value={text}
+            onChange={handleTextChange}
+            placeholder='Enter your prayer request here'
+            className='h-[150px] w-full px-[24px] py-[12px] border-[1px] border-[#9A4797] text-[#9A4797] rounded-md my-[42px] font-EuclidMedium outline-none text-justify'
+          />
+          <button
+            onClick={() => {
+              setSubmit(false)
+              setSubmitted(true)
+            }}
+            className='w-full cursor-pointer px-[24px] py-[12px] bg-[#9A4797] text-[white] rounded-md my-[12px] font-EuclidMedium shadow-md'
+            disabled={!text && !selectedValue}
+          >
+            Submit
+          </button>
+        </div>
+      ) : (
+        ''
+      )}
+      {submitted ? (
+        <Popuprequest setSubmit={setSubmit} setSubmitted={setSubmitted} />
+      ) : (
+        ''
+      )}
     </div>
   )
 }
